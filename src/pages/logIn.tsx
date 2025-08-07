5fafbb57f636b0bb75dd2bd5b5cf6eda811f1505
Amandeepsinghn <amandeepsingh.kaillay@gmail.com>
2025-08-08 01:02:20 +0530

    added signupButton

--- src/pages/logIn.tsx
+++ src/pages/logIn.tsx
@@ -1,14 +1,19 @@
 import axios, { AxiosError } from "axios";
 import React, { useReducer, useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { AiOutlineLoading } from "react-icons/ai";
 
 const baseUrl = import.meta.env.VITE_ENDPOINT;
 
 export default function LogIn() {
   const [password, showPassword] = useState<boolean>(false);
 
+  const navigator = useNavigate();
+
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+
+  const [loading, setLoading] = useState<boolean>(false);
 
   const [showError, setError] = useState<boolean>(false);
 
@@ -43,19 +48,30 @@ export default function LogIn() {
             className="px-2 py-2 text-white bg-indigo-600 w-full rounded-md hover:bg-indigo-700 block text-center cursor-pointer"
             onClick={async () => {
               try {
+                setLoading(true);
                 const response = await axios.post(`${baseUrl}/api/login`, {
                   email: emailRef.current?.value,
                   password: passwordRef.current?.value,
                 });
+                localStorage.setItem("token", response.data.body);
+                navigator("/dashboard");
               } catch (error: unknown) {
                 const err = error as AxiosError;
-
-                setError(!showError);
+                setError(true);
+              } finally {
+                setLoading(false);
               }
             }}
           >
-            Log In
+            {loading ? (
+              <div className="flex justify-center">
+                <AiOutlineLoading />{" "}
+              </div>
+            ) : (
+              "LogIn"
+            )}
           </button>
+          {showError && <div className="text-red-500 text-center">User does not exsist</div>}
           <div className="flex justify-center pt-4 space-x-1.5">
             <div className=" text-gray-600 text-sm">Don't have an account ?</div>
             <Link className="text-sm text-indigo-600" to={"/signup"}>
